4ffb5eb46fdcea4d6510a6b0356edb30987fb13e
John Paul <johnarpaul@gmail.com>
2019-12-02 10:47:43 +0000

    Update poker.rb

--- poker.rb
+++ poker.rb
@@ -2,27 +2,50 @@ require 'rubycards'
 include RubyCards
 
 start = true
+flop = false
+
+deck = Deck.new
 
 while start == true do
   command = gets.to_s.downcase.delete!("\n")
-
-  hand = Hand.new
-  deck = Deck.new
   deck.shuffle!
 
+
   if command == "hole"
+    deck.shuffle!
+    hand = Hand.new
     hand.draw(deck, 2)
     puts hand
   elsif command == "flop"
-    hand.draw(deck, 3)
-    puts hand
-  elsif command == "turn"
+    deck.shuffle!
+    @flop = Hand.new
+    @flop.draw(deck, 3)
+    puts @flop
+    flop = true
+  elsif command == "turn" && flop == false
+    turn = Hand.new
     hand.draw(deck, 4)
     puts hand
-  elsif command == "river"
+  elsif command == "turn" && flop == true
+    @turn = Hand.new
+    @turn.draw(deck, 1)
+    puts @flop
+    puts @turn
+    puts deck.count
+  elsif command == "river" && flop == false
+    river = Hand.new
     hand.draw(deck, 5)
     puts hand
-  else
+  elsif command == "river" && flop == true
+    @river = Hand.new
+    @river.draw(deck, 1)
+    puts @flop
+    puts @turn
+    puts @river
+  elsif command == "exit"
     start = false
+  elsif command == "reset"
+    flop = false
+    deck.shuffle!
   end
 end
